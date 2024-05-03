662b62aa535ec72429e6488dd18bbdcdf7216362
Zek <zek@pop-os.localdomain>
2024-05-03 15:37:06 -0400

    Class for interacting with the Google Cal API

--- src/website_scraper/google_calendar.py
+++ src/website_scraper/google_calendar.py
@@ -22,74 +22,82 @@ import os
 # https://developers.google.com/calendar/api/quickstart/python
 # https://developers.google.com/resources/api-libraries/documentation/calendar/v3/python/latest/calendar_v3.events.html#list
 
-
-def getCalenderReadClient():
-    SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
-    credentialTokens = None
-    credential_token_path = f"{os.getcwd()}/src/token.json"
-    if os.path.exists(credential_token_path):
-        credentialTokens = Credentials.from_authorized_user_file(credential_token_path, SCOPES)
+class GCalAPI:
+    _apiClient: Resource
     
-    if not credentialTokens or not credentialTokens.valid:
-        if credentialTokens and credentialTokens.expired and credentialTokens.refresh_token:
-            credentialTokens.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                    f"{os.getcwd()}/src/OAuthClientApp.json", SCOPES
+    def __init__(self):
+        self.getCalenderReadClient()
+    
+    def _initCalendarReadClient(self):
+        SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+        credentialTokens = None
+        credential_token_path = f"{os.getcwd()}/src/token.json"
+        if os.path.exists(credential_token_path):
+            credentialTokens = Credentials.from_authorized_user_file(credential_token_path, SCOPES)
+        
+        if not credentialTokens or not credentialTokens.valid:
+            if credentialTokens and credentialTokens.expired and credentialTokens.refresh_token:
+                credentialTokens.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                        f"{os.getcwd()}/src/OAuthClientApp.json", SCOPES
+                    )
+                credentialTokens = flow.run_local_server(port=9000)
+            
+            # When refreshed authentication token needs to be re-written, and if authenticating for the first time it needs to be just written
+            with open(credential_token_path, "w") as tokenFile:
+                tokenFile.write(credentialTokens.to_json())
+            
+        
+        self._apiClient = build("calendar", "v3", credentials=credentialTokens)
+
+    def getAllEventsAWeekFromNow(self, calendarId: str, dateOfLastEventScraped: datetime = None) -> [EventType]:
+        """Get events all events for that specific calender a week from today.
+
+        Args:
+            service (Resource): Calender resource available from creating a client with 'read calender' scope
+            calendarId (str): UUID of calender. Same ID used to subscribe to a calender
+            dateOfLastEventScraped (str, optional): UTC date with timedelta following isoformat. Have to add 'Z' to isoformat to indicate its UTC time. Defaults to None.
+        """
+        try:
+            # Call the Calendar API, 'Z' indicates UTC time
+            dateOfLastEventScrapedString = datetime.utcnow().isoformat() + "Z" if dateOfLastEventScraped is None else dateOfLastEventScraped.isoformat() + "Z"
+            weekFromNow = datetime.utcnow() + timedelta(days=7)
+            weekFromNow = weekFromNow.isoformat() + "Z"
+            
+            events_result = (
+                self._apiClient.events()
+                .list(
+                    calendarId=calendarId,
+                    timeMin=dateOfLastEventScrapedString,
+                    timeMax=weekFromNow,
+                    singleEvents=True,
+                    orderBy="startTime",
                 )
-            credentialTokens = flow.run_local_server(port=9000)
-        
-        # When refreshed authentication token needs to be re-written, and if authenticating for the first time it needs to be just written
-        with open(credential_token_path, "w") as tokenFile:
-            tokenFile.write(credentialTokens.to_json())
-        
-    
-    return build("calendar", "v3", credentials=credentialTokens)
-
-def getAllEventsAWeekFromNow(service: Resource, calendarId: str, dateOfLastEventScraped: str = None) -> [EventType]:
-    """Get events all events for that specific calender a week from today.
-
-    Args:
-        service (Resource): Calender resource available from creating a client with 'read calender' scope
-        calendarId (str): UUID of calender. Same ID used to subscribe to a calender
-        dateOfLastEventScraped (str, optional): UTC date with timedelta following isoformat. Have to add 'Z' to isoformat to indicate its UTC time. Defaults to None.
-    """
-    try:
-        # Call the Calendar API, 'Z' indicates UTC time
-        dateOfLastEventScraped = datetime.utcnow().isoformat() + "Z" if dateOfLastEventScraped is None else dateOfLastEventScraped
-        weekFromNow = datetime.utcnow() + timedelta(days=7)
-        weekFromNow = weekFromNow.isoformat() + "Z"
-        
-        events_result = (
-            service.events()
-            .list(
-                calendarId=calendarId,
-                timeMin=dateOfLastEventScraped,
-                timeMax=weekFromNow,
-                singleEvents=True,
-                orderBy="startTime",
+                .execute()
             )
-            .execute()
-        )
-        googleEvents = events_result.get("items", [])
-        if not googleEvents:
-            print("No upcoming events found.")
-            return
+            googleEvents = events_result.get("items", [])
+            if not googleEvents:
+                print("No upcoming events found.")
+                return
 
-        events = []
-        for googleEvent in googleEvents:
-            eventAddress = _parse_google_location(googleEvent.get("location"))
-            event = EventType(attributedToId=0, 
-                              title= googleEvent["summary"], description=googleEvent.get("description"),
-                              beginsOn=googleEvent["start"].get("dateTime"),
-                              endsOn=googleEvent["end"].get("dateTime"),
-                              onlineAddress="", physicalAddress=eventAddress,
-                              category=None, tags=None)
-            events.append(event)
-        
-        return events
-    except HttpError as error:
-        print(f"An error occurred: {error}")
+            events = []
+            for googleEvent in googleEvents:
+                eventAddress = _parse_google_location(googleEvent.get("location"))
+                event = EventType(attributedToId=0, 
+                                title= googleEvent["summary"], description=googleEvent.get("description"),
+                                beginsOn=googleEvent["start"].get("dateTime"),
+                                endsOn=googleEvent["end"].get("dateTime"),
+                                onlineAddress="", physicalAddress=eventAddress,
+                                category=None, tags=None)
+                events.append(event)
+            
+            return events
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+    
+    def close(self):
+        self._apiClient.close()
 
 def _parse_google_location(location:str):
     return None if location is None else None
