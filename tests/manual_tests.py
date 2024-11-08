9a1dd8da400e08a25f6a04d06e326e212fd04834
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-11-07 21:21:31 -0500

    Uploading Files And Using it With Events

--- tests/manual_tests.py
+++ tests/manual_tests.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import logging
 import os
@@ -9,27 +10,26 @@ from src.db_cache import UploadedEventRow, SQLiteDB, UploadSource, ScraperTypes
 from src.parser.types.generics import GenericEvent
 from src.parser.types.submission_handlers import GroupEventsKernel
 from src.publishers.mobilizon.api import MobilizonAPI
-from src.publishers.mobilizon.types import EventParameters
+from src.publishers.mobilizon.types import EventParameters, MobilizonEvent
 from src.scrapers.google_calendar.api import GCalAPI
 from src.scrapers.ical.scraper import ICALScraper
 
-endpoint = "https://ctgrassroots.org/graphiql"
-secrets: dict = None
-# with open(f"{os.getcwd()}/src/secrets.json", "r") as f:
-#     secrets = json.load(f)
+endpoint = os.environ.get("MOBILIZON_ENDPOINT")
+email = os.environ.get("MOBILIZON_EMAIL")
+passwd = os.environ.get("MOBILIZON_PASSWORD")
 
 
-def manualTestCreation():
+def manual_test_creation():
 
-    mobilizonAPI = MobilizonAPI(endpoint, secrets["email"], secrets["password"])
+    mobilizon_api = MobilizonAPI(endpoint, email, passwd)
     # Time object requires timeZone
-    beginsOn = datetime(2024, 7, 7, 14, 35, tzinfo=timezone.utc)
-    endsOn = datetime(2024, 7, 7, 17, 45, tzinfo=timezone.utc)
+    begins_on = datetime(2024, 7, 7, 14, 35, tzinfo=timezone.utc)
+    ends_on = datetime(2024, 7, 7, 17, 45, tzinfo=timezone.utc)
     geo_locator = Nominatim(user_agent="manual test creation")
     location = geo_locator.geocode("250 State St, New Haven, CT 06510").raw
     geom = f"{location['lon']};{location['lat']}"
     print(geom)
-    cafe9Local = EventParameters.Address(locality="New Haven", 
+    cafe9_local = EventParameters.Address(locality="New Haven",
                                          postalCode="06510", street="250 State St", 
                                          country="United States", geom=geom,
                                          originId=f"nominatim:{location['place_id']}",
@@ -39,55 +39,62 @@ def manualTestCreation():
     # with open("//home/zek/Documents/Code/CTEventScraper/src/Duck.jpg", "rb") as f:
     #     params = {"file": f}
     #     print(mobilizonAPI.upload_file("Duck", f))
-    mobilizonAPI.logout()
+    mobilizon_api.logout()
     print("Login and out")
 
-def manualTestGoogleCalendar(printEvents:bool = False):
+def manual_test_file_upload():
+    mobilizon_api = MobilizonAPI(endpoint, email, passwd)
+    event = MobilizonEvent(25, "Test", "Description", "2025-05-27T00:00:00-04:00")
+    result = mobilizon_api.upload_file("https://cdn.britannica.com/92/100692-050-5B69B59B/Mallard.jpg")
+    event.picture = EventParameters.MediaInput(result)
+    mobilizon_api.bot_created_event(event)
+
+def manual_test_google_calendar(print_events:bool = False):
     google_calendar_api = GCalAPI()
     google_calendars: dict = None
     with open(f"{os.getcwd()}/src/scrapers/GCal.json", "r") as f:
         google_calendars = json.load(f)
 
     bsbco = google_calendars["Bradley Street Bike Co-Op"]
-    exampleTimeOffset = datetime.utcnow() + timedelta(days=3)
-    exampleTimeOffset = exampleTimeOffset.isoformat() + "Z"
+    example_time_offset = datetime.utcnow() + timedelta(days=3)
+    example_time_offset = example_time_offset.isoformat() + "Z"
     db = SQLiteDB(True)
-    allEvents = google_calendar_api.getAllEventsAWeekFromNow(
+    all_events = google_calendar_api.getAllEventsAWeekFromNow(
         calendarDict=bsbco, calendarId=bsbco["googleIDs"][0], 
         checkCacheFunction=db.entry_already_in_cache)
     
-    if(printEvents):
-        for event in allEvents:
+    if(print_events):
+        for event in all_events:
             print(f"Start: {event.beginsOn}, End: {event.endsOn}, Title: {event.title}\nDescription: {event.description}, Location: {None if event.physicalAddress is None else event.physicalAddress.locality}")
 
-    return allEvents
+    return all_events
 
 
-def manualTestCacheDB():
-    allEvents: [UploadedEventRow] = [
+def manual_test_cache_db():
+    all_events: [UploadedEventRow] = [
         UploadedEventRow("uuid1", "id1", "title1", "2022-05-05T16:00:00-04:00", "2", "group2"),
         UploadedEventRow("uuid5", "id1", "title1", "2022-05-05T10:00:00-04:00", "2", "group2"),
         UploadedEventRow("uuid3", "id1", "title1", "2022-05-04T10:00:00-04:00", "2", "group2")
     ]
-    eventSources: [UploadSource] = [
+    event_sources: [UploadSource] = [
         UploadSource("uuid1", "website", "123", ScraperTypes.GOOGLE_CAL),
         UploadSource("uuid5", "website", "123", ScraperTypes.GOOGLE_CAL),
         UploadSource("uuid3", "website", "123", ScraperTypes.GOOGLE_CAL)
     ]
     db = SQLiteDB(True)
-    for k in range(len(allEvents)):
-        db.insert_uploaded_event(allEvents[k], eventSources[k])
+    for k in range(len(all_events)):
+        db.insert_uploaded_event(all_events[k], event_sources[k])
     # print(db.selectAllFromTable().fetchall())
     print(db.get_last_event_date_for_source_id("123").isoformat())
     db.close()
 
-def getEventBotInfo():
-    mobilizonAPI = MobilizonAPI(endpoint, secrets["email"], secrets["password"])
-    print(mobilizonAPI.getActors())
-    print(mobilizonAPI.getGroups())
-    mobilizonAPI.logout()
+def get_event_bot_info():
+    mobilizon_api = MobilizonAPI(endpoint, email, passwd)
+    print(mobilizon_api.getActors())
+    print(mobilizon_api.getGroups())
+    mobilizon_api.logout()
 
-def _getAddressInfo():
+def _get_address_info():
     calendar_locations = {
         "Cafe Nine": "250 State St, New Haven, CT 06510",
         "BSBCO": "138 Bradley St, New Haven, CT 06511",
@@ -118,7 +125,8 @@ def manual_test_ical():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
-    manual_test_ical()
+    # manual_test_ical()
+    manual_test_file_upload()
     # manualTestCreation()
     # manualTestGoogleCalendar(False)
     # manualTestCacheDB()
